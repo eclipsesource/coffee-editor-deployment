327eafab00e23e78a5816f8618e764fd961ab117
Lucas Koehler <lkoehler@eclipsesource.com>
2020-11-05 15:17:09 +0100

    Improve configurability and reusability
    Make the following properties configurable
    * kubernetes namespace
    * Image name and version of instance docker images
    * Image registry
    * Host
    * Prefix for created kubernetes objects (services, jobs, pods, etc)
    
    Add current values to the environment configuration and as default
    values in ExampleServer.
    
    Rename launch methods and exception to not contain coffee editor.

--- launch.sh
+++ launch.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-echo Echo environment 1.COFFEE_EDITOR 2.MAX_CONCURRENT_USERS 3.MIN_AVAILABLE_SERVICES 4.JOB_CLEANUP_MINUTES 5.JOB_TTL_SECONDS 6.CLEANUP_HOURS 7.DEBUGGING 8.INGRESS_LIMIT 9.EGRESS_LIMIT 10.PARALLEL_SPAWN
-echo $COFFEE_EDITOR
+echo Echo environment 1.NAMESPACE 2.IMAGE_NAME 3.IMAGE_VERSION 4.IMAGE_REGISTRY 5.MAX_CONCURRENT_USERS 6.MIN_AVAILABLE_SERVICES 7.JOB_CLEANUP_MINUTES 8.JOB_TTL_SECONDS 9.CLEANUP_HOURS 10.DEBUGGING 11.INGRESS_LIMIT 12.EGRESS_LIMIT 13.PARALLEL_SPAWN 14.HOST 15.PREFIX
+echo $NAMESPACE
+echo $IMAGE_NAME
+echo $IMAGE_VERSION
+echo $IMAGE_REGISTRY
 echo $MAX_CONCURRENT_USERS
 echo $MIN_AVAILABLE_SERVICES
 echo $JOB_CLEANUP_MINUTES
@@ -11,5 +14,7 @@ echo $DEBUGGING
 echo $INGRESS_LIMIT;
 echo $EGRESS_LIMIT;
 echo $PARALLEL_SPAWN;
+echo $HOST;
+echo $PREFIX;
 
-java -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Dorg.osgi.service.http.port=9091 -Dorg.eclipse.rwt.clientLibraryVariant=DEBUG -Dorg.eclipse.equinox.http.jetty.log.stderr.threshold=info -Dcoffee.editor=$COFFEE_EDITOR -Dmax.concurrent.users=$MAX_CONCURRENT_USERS -Dmin.available.services=$MIN_AVAILABLE_SERVICES -Djob.cleanup.minutes=$JOB_CLEANUP_MINUTES -Djob.ttl.seconds=$JOB_TTL_SECONDS -Dcleanup.hours=$CLEANUP_HOURS -Dingress.limit=$INGRESS_LIMIT -Degress.limit=$EGRESS_LIMIT -Dparallel.spawn=$PARALLEL_SPAWN -Ddebugging=$DEBUGGING -jar /coffee-editor//plugins/org.eclipse.equinox.launcher_1.5.500.v20190715-1310.jar -os linux -ws gtk -arch -showsplash -name Eclipse -startup /coffee-editor//plugins/org.eclipse.equinox.launcher_1.5.500.v20190715-1310.jar --launcher.overrideVmargs -exitdata 888021 -consoleLog -console 
+java -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Dorg.osgi.service.http.port=9091 -Dorg.eclipse.rwt.clientLibraryVariant=DEBUG -Dorg.eclipse.equinox.http.jetty.log.stderr.threshold=info -Dnamespace=$NAMESPACE -Dimage.name=$IMAGE_NAME -Dimage.version=$IMAGE_VERSION -Dimage.registry=$IMAGE_REGISTRY -Dmax.concurrent.users=$MAX_CONCURRENT_USERS -Dmin.available.services=$MIN_AVAILABLE_SERVICES -Djob.cleanup.minutes=$JOB_CLEANUP_MINUTES -Djob.ttl.seconds=$JOB_TTL_SECONDS -Dcleanup.hours=$CLEANUP_HOURS -Dingress.limit=$INGRESS_LIMIT -Degress.limit=$EGRESS_LIMIT -Dparallel.spawn=$PARALLEL_SPAWN -Ddebugging=$DEBUGGING -Dhost=$HOST -Dprefix=$PREFIX -jar /coffee-editor//plugins/org.eclipse.equinox.launcher_1.5.500.v20190715-1310.jar -os linux -ws gtk -arch -showsplash -name Eclipse -startup /coffee-editor//plugins/org.eclipse.equinox.launcher_1.5.500.v20190715-1310.jar --launcher.overrideVmargs -exitdata 888021 -consoleLog -console 
